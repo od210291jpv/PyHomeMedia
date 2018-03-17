d31b235b20f27acfd5d49aa8d1611abaefb5762e
od210291jpv <od210291jpv@gmail.com>
2018-03-18 00:51:07 +0200

    Added new HTML template, updated Bootstrap framework. ORM: Added new table - Playlist(experimantal)

--- source/src/Home/Home/Application/models.py
+++ source/src/Home/Home/Application/models.py
@@ -3,6 +3,7 @@ from Home.settings import MEDIA_ROOT
 
 # Create your models here.
 
+
 class AudioFile(models.Model):
 
     class Meta(object):
@@ -35,9 +36,21 @@ class AudioFile(models.Model):
     def __unicode__(self):
         return u'%s %s %s' % (self.path, self.name, self.author)
 
+    class Playlist(models.Model):
 
-    # class Playlist(models.Model):
-    #
-    #     class Meta(object):
-    #         verbose_name = u'Playlist'
-    #         verbose_name_plural = u'Playlists'
+        class Meta(object):
+            verbose_name = u'Playlist'
+            verbose_name_plural = u'Playlists'
+
+        name = models.CharField(
+            max_length=256,
+            blank=False,
+            verbose_name=u'Playlist name'
+        )
+
+        song = models.ManyToManyField('AudioFile',
+            null=True,
+            blank=True,
+            verbose_name=u'Song name',
+
+                                    )
