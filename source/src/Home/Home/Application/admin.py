bd8837f4ea7658c6ddd1033c0de38eb9eddac285
od210291jpv <od210291jpv@gmail.com>
2018-03-18 01:10:18 +0200

    Registered Playlist model

--- source/src/Home/Home/Application/admin.py
+++ source/src/Home/Home/Application/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import AudioFile
+from .models import Playlist
 
 # Register your models here.
 admin.site.register(AudioFile)
+admin.site.register(Playlist)
