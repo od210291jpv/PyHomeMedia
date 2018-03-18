77553848dd78bcbabacac15fa3fe09847374bb38
od210291jpv <od210291jpv@gmail.com>
2018-03-18 15:05:32 +0200

    Added view playlist links in playlists template

--- source/src/Home/Home/Application/admin.py
+++ source/src/Home/Home/Application/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from .models import AudioFile
 from .models import Playlist
 
+@admin.register(AudioFile, Playlist)
+class AudioFileAdmin(admin.ModelAdmin):
+    date_hierarchy = 'pub_date'
+
 # Register your models here.
 admin.site.register(AudioFile)
 admin.site.register(Playlist)
