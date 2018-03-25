041dc437f32aa3b5cc8788d15ffd93ab7ec098b4
od210291jpv <od210291jpv@gmail.com>
2018-03-25 10:54:25 +0200

    Created Mysql connection

--- source/src/Home/Home/settings.py
+++ source/src/Home/Home/settings.py
@@ -80,8 +80,13 @@ WSGI_APPLICATION = 'Home.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': 'my.cnf',
+        'NAME': 'media',
+        'USER': 'root',
+        'PASSWORD': 'Password1991',
+        'HOST': '192.168.0.103',
+        'PORT': '3306'
     }
 }
 
