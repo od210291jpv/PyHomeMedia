c08bb8239f9f822517d39c612d0b634385cb440f
od210291jpv <od210291jpv@gmail.com>
2018-03-25 15:23:47 +0200

    Modified models fro propre playlists implementation

--- source/src/Home/Home/Application/models.py
+++ source/src/Home/Home/Application/models.py
@@ -59,7 +59,7 @@ class Playlist(models.Model):
         null=True,
         blank=True,
         through='PlayListRelations',
-        through_fields=('playlist', 'track'),
+        # through_fields=('playlist', 'track'),
         verbose_name=u'Songs',
         related_name=u'Playlists',
         related_query_name=u'playlist'
