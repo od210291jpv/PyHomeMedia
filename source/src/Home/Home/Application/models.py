896c3014dc8e5a0ed252b19e1736cdc503ff38fc
od210291jpv <od210291jpv@gmail.com>
2018-03-25 14:38:43 +0200

    Modified models fro propre playlists implementation

--- source/src/Home/Home/Application/models.py
+++ source/src/Home/Home/Application/models.py
@@ -55,7 +55,7 @@ class Playlist(models.Model):
         verbose_name=u'Playlist name'
         )
 
-    songs = models.ForeignKey('AudioFile',
+    songs = models.ManyToManyField('AudioFile',
         null=True,
         blank=True,
         verbose_name=u'Songs',
