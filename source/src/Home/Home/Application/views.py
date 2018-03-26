e11f44da47c55dffa187f59846dd92822b0ced63
od210291jpv <od210291jpv@gmail.com>
2018-03-26 18:42:29 +0200

    Modified models fro propre playlists implementation

--- source/src/Home/Home/Application/views.py
+++ source/src/Home/Home/Application/views.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
 from django.http import HttpResponse
-from models import AudioFile, Playlist
+from models import AudioFile, Playlist, PlayListRelations
 from Home.settings import PORTAL_URL
 
 # Create your views here.
 
 
 def show_playlist(request, plid):
-    return HttpResponse('<h1>playlist id: %s </h1>' % plid)
+    playlist_tracks = PlayListRelations.objects.all().filter(playlist_id=plid)
+    return HttpResponse('<h1>playlist id: %s </h1><br> %s <h2></2>' % plid, playlist_tracks)
 
 
 def playlists(request):
