5c42e8354dc1671f978bff84055d72a9c3365a73
od210291jpv <od210291jpv@gmail.com>
2018-03-26 19:16:05 +0200

    Added playlist tracks displaying basic view

--- source/src/Home/Home/Application/views.py
+++ source/src/Home/Home/Application/views.py
@@ -9,7 +9,7 @@ from Home.settings import PORTAL_URL
 
 def show_playlist(request, plid):
     playlist_tracks = PlayListRelations.objects.all().filter(playlist_id=int(plid))
-    return HttpResponse('<h1>playlist id: %s </h1> <br> <h2> %s <h2>') % (plid, playlist_tracks)
+    return HttpResponse('<h1>playlist id: %s </h1> <br> <h2> %s <h2>') % (plid, str(playlist_tracks))
 
 
 def playlists(request):
