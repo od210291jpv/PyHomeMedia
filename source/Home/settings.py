aef40b785af0d2b2dd575793b7f4ac879c4d693a
od210291jpv <od210291jpv@gmail.com>
2018-10-30 16:17:58 +0000

    added missing comma in DB settings

--- source/Home/settings.py
+++ source/Home/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         # 'HOST': 'localhost',
         # 'PORT': '3306'
 	# SQLite
-	'ENGINE': 'django.db.backends.sqlite3'
+	'ENGINE': 'django.db.backends.sqlite3',
 	'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
     }
 }
