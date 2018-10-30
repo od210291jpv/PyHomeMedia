d9d427ed21912f2170dc6f9fc928c1ec5c695b1c
od210291jpv <od210291jpv@gmail.com>
2018-10-30 14:55:32 +0000

    MySQL replaced to SQLite3

--- source/Home/settings.py
+++ source/Home/settings.py
@@ -80,13 +80,16 @@ WSGI_APPLICATION = 'Home.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        # 'OPTIONS': 'my.cnf',
-        'NAME': 'media',
-        'USER': 'user',
-        'PASSWORD': 'Password',
-        'HOST': 'localhost',
-        'PORT': '3306'
+	# MySQL
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': 'media',
+        # 'USER': 'user',
+        # 'PASSWORD': 'Password',
+        # 'HOST': 'localhost',
+        # 'PORT': '3306'
+	# SQLite
+	'ENGINE': 'django.db.backends.sqlite3'
+	'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
     }
 }
 
